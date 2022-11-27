c5b3e23a270c7dd12e9c24c579c571299a5005c2
thanknoah <78211701+thanknoah@users.noreply.github.com>
2022-11-27 09:45:55 +0000

    added some features lol

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Imports
-const { Client, Message } = require('discord.js-selfbot-v13');
+const { Client } = require('discord.js-selfbot-v13');
+const chalk = require("chalk");
 const client = new Client({
     checkUpdate: false
 });
@@ -8,50 +9,73 @@ const fs = require('fs');
 
 // Config Variables
 let prefix = "";
+let response = "Yes";
+let logCommands = "Yes";
+let userId = null;
+chalk.level = 1;
 
 // On Ready && Procedure
 client.on("ready", () => {
-    console.log(`Logged in: ${client.user.username}`);
+    console.log(chalk.redBright("🐨☯  ŇＯ𝓪ħş ѕ𝔢ᒪƑβⓞ𝕋 𝐯Ѳ.０❶  ♪♜"));
+    console.log(chalk.redBright("🐨☯  ŇＯ𝓪ħş ѕ𝔢ᒪƑβⓞ𝕋 𝐯Ѳ.０❶  ♪♜"));
+    console.log(chalk.redBright("🐨☯  ŇＯ𝓪ħş ѕ𝔢ᒪƑβⓞ𝕋 𝐯Ѳ.０❶  ♪♜"));
+    console.log(chalk.redBright("🐨☯  ŇＯ𝓪ħş ѕ𝔢ᒪƑβⓞ𝕋 𝐯Ѳ.０❶  ♪♜"));
 
-    const setPrefix = prompt("Prefix > ");
-    const setActivity = prompt("[Skip if not set] Set activity >");
-    const setActivityType = prompt("Activity Type > ")
+    console.log(chalk.green(`Logged in: ${client.user.username}`));
+    console.log(chalk.green(`User ID: ${client.user.id}`))
+
+    const setPrefix = prompt(chalk.redBright("Prefix > "));
+    const setActivity = prompt(chalk.redBright("[Skip if not set] Set activity > "));
+    const setActivityType = prompt(chalk.redBright("Activity Type > "));
+    const setRespondToCommand = prompt(chalk.redBright("Bot responds [Yes, No] > "));
+    const setCommandLog = prompt(chalk.redBright("Log commands [Yes, No] > "));
+
+    console.log(" ")
+
+    response = setRespondToCommand;
+    logCommands = setCommandLog;
 
     try {
         (setActivity) ? client.user.setActivity(setActivity, { activity: setActivityType }) : "";
     } catch (err) {
-        console.log(`Error: ${err}`)
+        console.log(chalk.red(`Error: ${err}`))
     }
 
+    userId = client.user.id;
     prefix = setPrefix;
 });
 
 // On Message
 client.on("message", (msg) => {
     if (!msg.content.startsWith(prefix)) return false;
-    let string = msg.content.split(/\b(\s)/)
-    string[0] = string[0].substring(1, string[0].length)
 
-    console.log(string)
+    let string = msg.content.split(" ");
+    let command = string[0].substring(1, string[0].length);
+    let arg1 = string[1];
+    let groupedArgs = ""
 
-    if (string[0] == "hello") {
+    string.forEach((v, i) => {
+        if (i != 0) groupedArgs = groupedArgs + ` ${v}` 
+    })
+
+    if (command == "hello") {
         msg.reply("Hello!");
     }
 
-    if (string[0] == "help") {
+    if (command == "help") {
         msg.reply("`Available Commands: !hello, !help`")
     }
 
-    if (string[0] == "setAboutMe") {
-        if (client.user.setAboutMe(string[2])) {
-            console.log("Done")
-        }
+    if (command == "setAboutMe" && msg.author.id == userId) {
+        client.user.setAboutMe(groupedArgs)
+        if (response == "Yes" || response == "yes") msg.reply(`Set about me to: ${string[2]}`)
+        if (logCommands == "Yes" || response == "yes") return console.log(chalk.green("LOGGED COMMAND: Changed about me!"))
     }
 })
 
 // Login
 client.login(fs.readFileSync("token.txt", "utf-8", (err) => {
     if (err) {
-        return console.log(err);
+        return console.log(chalk.red(err));
     }
 }));
