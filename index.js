920019da1ae03bf8bf37c0539fead2e9dd07d1b5
thanknoah <78211701+thanknoah@users.noreply.github.com>
2022-11-27 10:13:55 +0000

    added joke feature (warning jokes arent funny all
    L + ratio

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const client = new Client({
     checkUpdate: false
 });
 const prompt = require('prompt-sync')();
+const axios = require('axios');
 const fs = require('fs');
 
 // Config Variables
@@ -38,7 +39,7 @@ client.on("ready", () => {
     try {
         (setActivity) ? client.user.setActivity(setActivity, { activity: setActivityType }) : "";
     } catch (err) {
-        console.log(chalk.red(`Error: ${err}`))
+        console.log(chalk.red(`Error: ${err}`));
     }
 
     userId = client.user.id;
@@ -63,13 +64,28 @@ client.on("message", (msg) => {
     }
 
     if (command == "help") {
-        msg.reply("`Available Commands: !hello, !help`")
+        msg.reply("Available Commands: `help, hello, setAboutMe, setActivity, joke`");
     }
 
     if (command == "setAboutMe" && msg.author.id == userId) {
         client.user.setAboutMe(groupedArgs)
-        if (response == "Yes" || response == "yes") msg.reply(`Set about me to: ${string[2]}`)
-        if (logCommands == "Yes" || response == "yes") return console.log(chalk.green("LOGGED COMMAND: Changed about me!"))
+        if (response == "Yes" || response == "yes") msg.reply(`Set about me too: ${groupedArgs}`);
+        if (logCommands == "Yes" || response == "yes") return console.log(chalk.green("LOGGED COMMAND: Changed about me!"));
+    }
+
+    if (command == "setActivity" && msg.author.id == userId) {
+        client.user.setActivity(groupedArgs)
+        if (response == "Yes" || response == "yes") msg.reply(`Set activity too: ${groupedArgs}`);
+        if (logCommands == "Yes" || response == "yes") return console.log(chalk.green("LOGGED COMMAND: Changed activity!"));
+    }
+
+    if (command == "joke") {
+        axios
+        .get("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt")
+        .then((response) => {
+            msg.reply(response.data);
+            console.log(chalk.green("Successfully told joke!"))
+        })
     }
 })
 
