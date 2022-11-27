ac75d139a0d9fdf6f9dd7037bde7ec2da36aa5b8
thanknoah <78211701+thanknoah@users.noreply.github.com>
2022-11-27 13:39:30 +0000

    E

--- run.bat
+++ run.bat
@@ -2,7 +2,14 @@
 
 echo "Make sure you have NodeJS installed."
 title Noah's SelfBot V1.2
+
 :START
+
+npm install axios
+npm install discord.js-selfbot-v13
+npm install prompt-sync
+npm install chalk
+
 node index.js
 
 goto START 
