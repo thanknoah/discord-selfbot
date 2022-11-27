cf508cf0b5c7d84742ad323b564d0b771c1588a1
thanknoah <78211701+thanknoah@users.noreply.github.com>
2022-11-27 10:41:37 +0000

    few stuff

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
+echo "You need too install NodeJS on your computer for this too work!"
+node -v
+
+npm install discord.js-selfbot-v13
+npm install chalk
+npm install prompt-sync
+npm install axios
+
 node index.js
